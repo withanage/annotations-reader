161e2fd2c757c8ac6b22518313a2881d045b2955
Dulip Withanage <dulip.withanage@gmail.com>
2014-06-26 16:50:44 +0200

    $1

--- ingest/ingest_specific_path.sh
+++ ingest/ingest_specific_path.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-for i in {1..1}
-do
 curl -X DELETE "http://pers31.ub.uni-heidelberg.de:8080/fedora/rest/.well-known/genid"
+
+for i in {1..10}
+do
 curl -X DELETE "http://pers31.ub.uni-heidelberg.de:8080/fedora/rest/de/uni-heidelberg/ub/digi/diglit/lehmann1756/0001/$i"
-curl -X PUT  "http://pers31.ub.uni-heidelberg.de:8080/fedora/rest/de/uni-heidelberg/ub/digi/diglit/lehmann1756/0001/$i"
+#curl -X PUT  "http://pers31.ub.uni-heidelberg.de:8080/fedora/rest/de/uni-heidelberg/ub/digi/diglit/lehmann1756/0001/$i"
 curl -X PUT -H "Content-Type: text/turtle" --data-binary "@open_object.ttl" "http://pers31.ub.uni-heidelberg.de:8080/fedora/rest/de/uni-heidelberg/ub/digi/diglit/lehmann1756/0001/$i"   
 
 #curl -X PUT  "http://serv21.ub.uni-heidelberg.de:8080/fedora/fedora/rest/de/uni-heidelberg/ub/digi/diglit/lehmann1756/0001/$i"
