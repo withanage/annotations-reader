ff780d09bece3e6fba837b8bbbf8e94f431c7ac5
Dulip Withanage <dulip.withanage@gmail.com>
2014-07-07 11:05:57 +0200

    modalclean

--- web/js/app.js
+++ web/js/app.js
@@ -21,7 +21,7 @@ var readerApps = angular.module('readerApp', ['ui.bootstrap', 'ngTouch'], functi
 
 var readerApp = angular.module('readerApp', ['ui.bootstrap', 'ngTouch']);
 
-readerApp.controller('MainCtrl', function($scope, fetchfedora) {
+readerApp.controller('MainCtrl', function($scope, fetchfedora, $modal, $log) {
     $scope.oneAtATime = true;
 
     $scope.status = {
@@ -52,10 +52,58 @@ readerApp.controller('MainCtrl', function($scope, fetchfedora) {
             });
         });
     };
+
+
+    //modal
+    $scope.items = ['item1', 'item2', 'item3'];
+
+    $scope.open = function() {
+
+        var modalInstance = $modal.open({
+            templateUrl: 'myModalContent.html',
+            controller: ModalInstanceCtrl,
+            size: 'lg',
+            resolve: {
+                items: function() {
+                    return $scope.items;
+                }
+            }
+        });
+
+        modalInstance.result.then(function(selectedItem) {
+            $scope.selected = selectedItem;
+        }, function() {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+
+
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+
+    var ModalInstanceCtrl = function($scope, $modalInstance, items) {
+
+        $scope.items = items;
+        $scope.selected = {
+            item: $scope.items[0]
+        };
+
+        $scope.ok = function() {
+            $modalInstance.close($scope.selected.item);
+        };
+
+        $scope.cancel = function() {
+            $modalInstance.dismiss('cancel');
+        };
+    };
+
+    //end modal
+
+
 });
 
 
-readerApp.factory('fetchfedora', function($q, $timeout, $http) {
+readerApp.factory('fetchfedora', function($q, $http) {
 
 
     var context = {
