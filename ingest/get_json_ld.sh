9f7b4c9d1291f1ddf09da9de25bdab36026a3ad7
Dulip Withanage <dulip.withanage@gmail.com>
2014-07-07 09:44:22 +0200

    tags-work

--- ingest/get_json_ld.sh
+++ ingest/get_json_ld.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in {1..1}
 do
-curl  "http://pers31.ub.uni-heidelberg.de:8080/fedora/rest/de/uni-heidelberg/ub/digi/diglit/lehmann1756/1/"  -H "Accept: application/ld+json "
+curl  "http://pers31.ub.uni-heidelberg.de:8080/fedora/rest/de/uni-heidelberg/ub/digi/diglit/lehmann1756/0001/1"  -H "Accept: application/ld+json "
 #echo "_________________________________________"
 #curl "http://pers31.ub.uni-heidelberg.de:8080/fedora/rest/de/uni-heidelberg/ub/digi/diglit/lehmann1756/0001/1/" -H "Accept: application/ld+json "
 #echo "_________________________________________"
